eeb1509368d0daa08c02663e19623c3a0b109663
Lea Rosema <terabaud@gmail.com>
2020-11-21 12:18:34 +0100

    feat: add cname on deploy

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,5 @@
 module.exports = {
   name: "Lea's WebGL world",
   description: 'Various WebGL demos by Lea Rosema',
-  url:
-    process.env.ELEVENTY_ENV === 'production'
-      ? 'https://terabaud.github.io/lea-codes/'
-      : '',
+  url: '',
 };
